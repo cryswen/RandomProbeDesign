20d60882f754b87b0d8a32b2c2fcf2ebe47a6ce6
nurffffff <38639722+cryswen@users.noreply.github.com>
2018-06-07 15:50:14 -0400

    Add files via upload

--- ProbeDesign_local.py
+++ ProbeDesign_local.py
@@ -1,9 +1,14 @@
 import os
 import pandas as pd
 
+
+print('Blasting qurey sequences against c.elegans W265 transcriptome...')
+
 command = 'blastn -query query.fa -db /Users/wenxu/blastdb/c_elegans.PRJNA13758.WS265.mRNA_transcripts.fa -task blastn -evalue 1000 -dust no -outfmt "10 qseqid sseqid qcovs pident" -max_target_seqs 100 >> elegans_blast.csv'
 
 os.system(command)
+
+print('Blasting qurey sequences against yeast S288C transcriptome...')
 
 command = 'blastn -query query.fa -db /Users/wenxu/blastdb/yeast_S288C_rna_genomic_1000.fasta -task blastn -evalue 1000 -dust no -outfmt "10 qseqid sseqid qcovs pident" -max_target_seqs 100 >> yeast_blast.csv'
 
@@ -28,8 +33,12 @@ max_qs = pd.merge(max_qs1,max_qs2, on='id')
 max_qs.columns = ['id', 'elegans_q','yeast_q']
 max_qs.to_csv('blast_summary.csv')
 
+print('Filtering blast results based on query coverage less than 50%...')
+
 filtered = max_qs[(max_qs['elegans_q']<=50)&(max_qs['yeast_q']<=50)]
 
 blast_results = filtered.merge(query, on='id', how = 'left')
 blast_results.to_csv('filtered_results.csv')
+
+print('All done! Enjoy your results!!!')
 
--- make_query.py
+++ make_query.py
@@ -3,8 +3,8 @@ import random
 # make fasta file with queries
 f = open('query.fa','w')
 fcsv = open('query.csv','w')
-j = 1
-while j <= 1000000:
+j = 0
+while j < 1000000:
     x = ''
     for i in range(30):
         x += random.choice('atgc')
@@ -12,5 +12,7 @@ while j <= 1000000:
         f.write('>random' + str(j) + '\n' + x +'\n')
         fcsv.write('random' + str(j) + ',' + x +'\n')
         j += 1
+        if j%100000 == 0:
+	        print(str(j) + ' random primers generated.')
 f.close()
 fcsv.close()
